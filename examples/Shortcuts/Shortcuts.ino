4db2d021ab2d10a8e313e9de6e3a96ec2a7bb576
Drew Folta <drew@folta.net>
2014-01-05 13:07:36 -0800

    shortcuts example

--- examples/Shortcuts/Shortcuts.ino
+++ examples/Shortcuts/Shortcuts.ino
@@ -1,10 +1,13 @@
 #include <TextDevices.h>
+#include <ShortcutsDevice.h>
 
 TextDevices::Devices devices;
+TextDevices::ShortcutsDevice shortcuts;
 
 void setup() {
     Serial.begin(9600);
     devices.setup(&Serial);
+    devices.registerDevice(&shortcuts);
 }
 
 void loop() {
